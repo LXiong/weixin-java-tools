c10662a6727100b448d4814ad9f46cb933f16e4e
Binary Wang <binarywang@gmail.com>
2017-06-10 23:50:09 +0800

    init

--- weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/util/json/WxMpGsonBuilder.java
+++ weixin-java-miniapp/src/main/java/cn/binarywang/wx/miniapp/util/json/WxMpGsonBuilder.java
@@ -1,16 +1,11 @@
 package cn.binarywang.wx.miniapp.util.json;
 
 import cn.binarywang.wx.miniapp.bean.*;
-import cn.binarywang.wx.miniapp.bean.datacube.WxDataCubeUserCumulate;
-import cn.binarywang.wx.miniapp.bean.datacube.WxDataCubeUserSummary;
 import cn.binarywang.wx.miniapp.bean.kefu.WxMpKefuMessage;
 import cn.binarywang.wx.miniapp.bean.material.*;
 import cn.binarywang.wx.miniapp.bean.result.*;
 import cn.binarywang.wx.miniapp.bean.template.WxMpTemplateIndustry;
 import cn.binarywang.wx.miniapp.bean.template.WxMpTemplateMessage;
-import com.github.binarywang.wx.miniapp.bean.*;
-import com.github.binarywang.wx.miniapp.bean.material.*;
-import com.github.binarywang.wx.miniapp.bean.result.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -33,8 +28,6 @@ public class WxMpGsonBuilder {
     INSTANCE.registerTypeAdapter(WxMpTemplateMessage.class, new WxMpTemplateMessageGsonAdapter());
     INSTANCE.registerTypeAdapter(WxMpSemanticQueryResult.class, new WxMpSemanticQueryResultAdapter());
     INSTANCE.registerTypeAdapter(WxMpOAuth2AccessToken.class, new WxMpOAuth2AccessTokenAdapter());
-    INSTANCE.registerTypeAdapter(WxDataCubeUserSummary.class, new WxMpUserSummaryGsonAdapter());
-    INSTANCE.registerTypeAdapter(WxDataCubeUserCumulate.class, new WxMpUserCumulateGsonAdapter());
     INSTANCE.registerTypeAdapter(WxMpMaterialUploadResult.class, new WxMpMaterialUploadResultAdapter());
     INSTANCE.registerTypeAdapter(WxMpMaterialVideoInfoResult.class, new WxMpMaterialVideoInfoResultAdapter());
     INSTANCE.registerTypeAdapter(WxMpMassNews.WxMpMassNewsArticle.class, new WxMpMassNewsArticleGsonAdapter());
@@ -46,12 +39,9 @@ public class WxMpGsonBuilder {
     INSTANCE.registerTypeAdapter(WxMpMaterialNewsBatchGetResult.WxMaterialNewsBatchGetNewsItem.class, new WxMpMaterialNewsBatchGetGsonItemAdapter());
     INSTANCE.registerTypeAdapter(WxMpMaterialFileBatchGetResult.class, new WxMpMaterialFileBatchGetGsonAdapter());
     INSTANCE.registerTypeAdapter(WxMpMaterialFileBatchGetResult.WxMaterialFileBatchGetNewsItem.class, new WxMpMaterialFileBatchGetGsonItemAdapter());
-    INSTANCE.registerTypeAdapter(WxMpCardResult.class, new WxMpCardResultGsonAdapter());
-    INSTANCE.registerTypeAdapter(WxMpCard.class, new WxMpCardGsonAdapter());
     INSTANCE.registerTypeAdapter(WxMpMassPreviewMessage.class, new WxMpMassPreviewMessageGsonAdapter());
     INSTANCE.registerTypeAdapter(WxMediaImgUploadResult.class, new WxMediaImgUploadResultGsonAdapter());
     INSTANCE.registerTypeAdapter(WxMpTemplateIndustry.class, new WxMpIndustryGsonAdapter());
-    INSTANCE.registerTypeAdapter(WxMpUserBlacklistGetResult.class, new WxUserBlacklistGetResultGsonAdapter());
   }
 
   public static Gson create() {
